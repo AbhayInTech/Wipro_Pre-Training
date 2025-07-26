ec590066b35b3c0d7096c38cb04170342014488e
AbhayInTech <abhayrana8778@gmail.com>
2025-07-26 20:57:23 +0530

    Practise-1 is updated on 26-07-2025

---
+++ Practice/Practice-1/IEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_1
+{
+    internal interface IEmployee
+    {
+        // creating the interface for Employee to be implement into the Employee class
+        int setID();
+        string setDepartment();
+        double setSalary();
+        string setAddress();
+        string setPhoneNumber();
+        string setEmail();
+    }
+}
