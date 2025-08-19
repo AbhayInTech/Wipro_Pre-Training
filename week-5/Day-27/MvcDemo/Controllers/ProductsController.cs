8da2e0db6253f224eb4905c0aaa527809435b8ca
AbhayInTech <abhayrana8778@gmail.com>
2025-08-19 16:24:19 +0530

    Day-27 code for ASP.net is updated for MVC

---
+++ week-5/Day-27/MvcDemo/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcDemo.Models;
+
+namespace MvcDemo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Laptop", Description = "High performance laptop", Price = 999.99M },
+                new Product { Id = 2, Name = "Smartphone", Description = "Latest model smartphone", Price = 499.99M },
+                new Product { Id = 3, Name = "Tablet", Description = "Portable tablet with stylus", Price = 299.99M }
+            };
+            return View(products);// Pass the list of products to the view
+        }
+
+        // for Create.cshtml
+        public IActionResult Create()
+        {
+            // Return the view for creating a new product
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                // Here you would typically save the product to a database
+                // For this example, we will just redirect to the Index action
+                return RedirectToAction("Index");
+            }
+            // If the model state is not valid, return the same view with validation errors
+            return View(product);
+        }
+    }
+}
---
+++ week-5/Day-27/MvcDemo/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿// Adding data validation for validation purposes
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcDemo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+
+
+        [Required (ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = string.Empty;
+
+
+
+        [Required(ErrorMessage = "Description is required.")]
+        public string Description { get; set; } = string.Empty;
+
+
+
+        [Range(0.01, 10000.00, ErrorMessage = "Price must be between Rs 0.01 and Rs 10,000.00.")]
+        public decimal Price { get; set; }
+    }
+}
