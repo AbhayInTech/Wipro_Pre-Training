92133d643d372a06ac6385eca0edcfb9b808ad36
AbhayInTech <abhayrana8778@gmail.com>
2025-08-19 16:48:28 +0530

    update in the Book Catalog create from dotnet MVC

---
+++ week-5/Day-27/BookCatalog/Models/Book.cs
@@ -0,0 +1,18 @@
+namespace BookCatalog.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        public string Author { get; set; }
+
+        public string genre { get; set; }
+        
+        
+    }
+}
+
+public class Error
