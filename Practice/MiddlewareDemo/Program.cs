de4469611e6bf8b751e97335ee802c6593a83625
AbhayInTech <abhayrana8778@gmail.com>
2025-08-21 09:56:37 +0530

    assignment 1  is updated in practise folder

--- Practice/MiddlewareDemo/Program.cs
+++ Practice/MiddlewareDemo/Program.cs
@@ -1,6 +1,70 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var csp = builder.Configuration["SecurityHeaders:ContentSecurityPolicy"];
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+
+// Logging middleware
+app.Use(async (context, next) =>
+{
+    logger.LogInformation("Request: {Method} {Path}", context.Request.Method, context.Request.Path);
+    await next.Invoke();
+    logger.LogInformation("Response: {StatusCode}", context.Response.StatusCode);
+});
+
+// Middleware 2: Simple Authentication check
+app.Use(async (context, next) =>
+{
+    if (context.Request.Query.TryGetValue("auth", out var authkey) && authkey == "secret")
+    {
+        await next();
+    }
+    else
+    {
+        context.Response.StatusCode = 401; // Unauthorized
+        await context.Response.WriteAsync("Unauthorized");
+    }
+});
+
+//http://localhost:5224/?auth=secret
+
+// CSP middleware
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("Content-Security-Policy", csp);
+    await next();
+});
+
+
+// Error simulation route
+app.Map("/throw", () =>
+{
+    throw new Exception("Simulated exception");
+});
+
+// Error handler
+app.UseExceptionHandler("/error");
+app.Map("/error", (HttpContext context) =>
+{
+    context.Response.ContentType = "text/html";
+    return context.Response.SendFileAsync("wwwroot/error.cshtml");
+});
+
+// Default route
+app.MapGet("/", async context =>
+{
+    context.Response.ContentType = "text/html";
+    await context.Response.SendFileAsync("wwwroot/index.cshtml");
+});
+
+app.MapFallback(async context =>
+{
+    context.Response.ContentType = "text/plain";
+    context.Response.StatusCode = 404;
+    await context.Response.WriteAsync("404 - Page not found");
+});
+
 
 app.Run();
